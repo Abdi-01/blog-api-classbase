3d86689197b6ce0893a99bc57116a30c77c09e0f
Abdi-01 <abdialghi@gmail.com>
2025-04-17 15:27:11 +0700

    revamp: redis

--- src/controllers/article.controller.ts
+++ src/controllers/article.controller.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import { prisma } from "../config/prisma";
+import redisClient from "../config/redis";
 
 class ArticleController {
   async getArticles(
@@ -8,9 +9,21 @@ class ArticleController {
     next: NextFunction
   ): Promise<any> {
     try {
-      //
+      // 1. Check data in redis
+      const articleRedis = await redisClient.get("articles");
 
+      // 2. If exist, use data from redis as a response
+      if (articleRedis) {
+        console.log("Data from redis");
+
+        return res.status(200).send(JSON.parse(articleRedis));
+      }
+
+      // 3. If not exist data in redis
       const results = await prisma.post.findMany();
+
+      // 4. Store data from prisma to redis
+      await redisClient.setEx("articles", 10, JSON.stringify(results));
 
       return res.status(200).send(results);
     } catch (error) {
